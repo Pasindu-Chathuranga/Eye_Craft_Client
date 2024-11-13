7180c2ea0cc8b3dda19f22dbfea8810198b5690f
Anubis-cmyk <it19099132@my.sliit.lk>
2024-11-13 12:01:55 +0530

    fixing home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Box, Typography, Button, Grid, Fade, Slide } from '@mui/material';
+import { Box, Typography, Button, Grid, Fade, Slide, useMediaQuery } from '@mui/material';
 import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-scroll'; // Import Link from react-scroll
 import homeBG from '../assets/herobg.JPG';
 import logo from '../assets/logos/logo-white.png';
+import theme from '../theme/Theme';
 
 function Home() {
   const { ref, inView } = useInView({
@@ -11,27 +12,29 @@ function Home() {
     threshold: 0.3, // Trigger when 30% of the component is visible
   });
 
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Box id="home" pt={1} pb={5} ref={ref}>
+    <Box id="home" pt={2} pb={isMobile ? 7 : 2} ref={ref}>
       <Fade in={inView} timeout={1000}>
         <Grid container spacing={2} alignItems="center">
-          {/* Left Column */} 
+          {/* Left Column */}
           <Grid item xs={12} md={6}>
             <Slide direction="left" in={inView} timeout={800}>
               <Box
                 component="img"
                 src={homeBG}
                 alt="Art in every blink"
-                sx={{ width: '100%',height:'95%', borderRadius: 2 }}
+                sx={{ width: '100%', borderRadius: 2 }}
               />
             </Slide>
           </Grid>
 
           {/* Right Column */}
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={6} >
             <Slide direction="right" in={inView} timeout={1000}>
-              <Box> 
-                <Typography variant="h2" gutterBottom sx={{ color: '#D3D4D9', marginLeft: '15px' }}>
+              <Box>
+                <Typography variant={isMobile ? "h4" : 'h2'} gutterBottom sx={{ color: '#D3D4D9', marginLeft: '15px', fontSize: isMobile ? "40px" : 'h2' }}>
                   Art in Every Blink
                 </Typography>
                 <Typography
@@ -59,7 +62,7 @@ function Home() {
               </Box>
             </Slide>
           </Grid>
-          
+
         </Grid>
       </Fade>
     </Box>
