bd0e0847eafa5fe400ec94bb53322414951c22df
Anubis-cmyk <it19099132@my.sliit.lk>
2025-04-09 20:48:07 +0530

    eye craft client v2 images

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -10,34 +10,34 @@ import {
   useTheme,
 } from "@mui/material";
 import { Close as CloseIcon, ArrowBack, ArrowForward } from "@mui/icons-material";
-import image1 from "../assets/gallary/1.jpg";
-import image2 from "../assets/gallary/2.png";
-import image3 from "../assets/gallary/3.jpg";
-import image4 from "../assets/gallary/4.jpg";
-import image5 from "../assets/gallary/5.jpg";
-import image6 from "../assets/gallary/6.jpg";
-import image7 from "../assets/gallary/7.jpg";
-import image8 from "../assets/gallary/8.JPG";
-import image9 from "../assets/gallary/9.JPG";
-import image10 from "../assets/gallary/10.JPG";
-import image11 from "../assets/gallary/11.JPG";
-import image12 from "../assets/gallary/12.JPG";
-import image13 from "../assets/gallary/13.JPG";
+// import image1 from "../assets/gallary/1.jpg";
+// import image2 from "../assets/gallary/2.png";
+// import image3 from "../assets/gallary/3.jpg";
+// import image4 from "../assets/gallary/4.jpg";
+// import image5 from "../assets/gallary/5.jpg";
+// import image6 from "../assets/gallary/6.jpg";
+// import image7 from "../assets/gallary/7.jpg";
+// import image8 from "../assets/gallary/8.JPG";
+// import image9 from "../assets/gallary/9.JPG";
+// import image10 from "../assets/gallary/10.JPG";
+// import image11 from "../assets/gallary/11.JPG";
+// import image12 from "../assets/gallary/12.JPG";
+// import image13 from "../assets/gallary/13.JPG";
 
 const images = [
-  { id: 1, src: image1, alt: "Image 1" },
-  { id: 2, src: image2, alt: "Image 2" },
-  { id: 3, src: image3, alt: "Image 3" },
-  { id: 4, src: image4, alt: "Image 4" }, 
-  { id: 5, src: image5, alt: "Image 5" },
-  { id: 6, src: image6, alt: "Image 6" },
-  { id: 7, src: image7, alt: "Image 7" },
-  { id: 8, src: image8, alt: "Image 8" },
-  { id: 9, src: image9, alt: "Image 9" }, 
+  // { id: 1, src: image1, alt: "Image 1" },
+  // { id: 2, src: image2, alt: "Image 2" },
+  // { id: 3, src: image3, alt: "Image 3" },
+  // { id: 4, src: image4, alt: "Image 4" }, 
+  // { id: 5, src: image5, alt: "Image 5" },
+  // { id: 6, src: image6, alt: "Image 6" },
+  // { id: 7, src: image7, alt: "Image 7" },
+  // { id: 8, src: image8, alt: "Image 8" },
+  // { id: 9, src: image9, alt: "Image 9" }, 
   // { id: 10, src: image10, alt: "Image 10" },
   // { id: 11, src: image11, alt: "Image 11" },
-  { id: 12, src: image12, alt: "Image 12" },
-  { id: 13, src: image13, alt: "Image 13" },
+  // { id: 12, src: image12, alt: "Image 12" },
+  // { id: 13, src: image13, alt: "Image 13" },
 ];
 
 const Gallery = () => {
